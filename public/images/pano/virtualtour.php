1aa4e5c705c47d7a01fca99ece39d8e7a9b9d29e
Daniil Maltsev <da-maltsev@inbox.ru>
2026-06-09 12:22:16 +0500

    Embed all images into build (from public_html), remove external URL deps

---
+++ public/images/pano/virtualtour.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <object width="100%" height="100%">
+            <param name="movie" value="FILE">
+            <embed src="virtualtour.swf" width="100%" height="100%">
+            </embed>
+        </object>
+    </body>
+</html>
